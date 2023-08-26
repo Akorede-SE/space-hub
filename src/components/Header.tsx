a0a681c6a5913ab0567797c02b9af3c424135cf7
akorede <emiolasaheed1@gmail.com>
2023-08-26 08:20:13 +0100

    Style Navlink actice state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,39 @@
 import { NavLink } from 'react-router-dom'
 import planet from '../assets/planet.png';
+import { CSSProperties } from 'react';
 const Header = () => {
+  const styles: CSSProperties = {
+    textDecoration: 'underline',
+  }
   return (
-    <header className=" bg-[whitesmoke] p-4">
+    <header className=" bg-[whitesmoke] p-4 flex justify-between">
       <section className=" flex gap-2 items-center">
         <img src={planet} alt="planet image" />
         <h2 className=" font-Montserrat text-2xl font-[400]">Space Travelers' Hub</h2>
       </section>
       <nav>
-        <ul>
+        <ul className=" flex gap-2">
           <li>
-            <NavLink to='/'>
+            <NavLink to='/' 
+            className=" text-blue-500"
+            style={({ isActive }) => isActive ? styles : {}}
+            >
               Rockets
             </NavLink>
           </li>
-          <li>
-            <NavLink to='/'>
+          <li className=" border-r px-2">
+            <NavLink to='missions'
+              className=" text-blue-500"
+              style={({ isActive }) => isActive ? styles : {}}
+            >
               Missions
             </NavLink>
           </li>
           <li>
-            <NavLink to='/'>
+            <NavLink to='/profile'
+              className=" text-blue-500"
+              style={({ isActive }) => isActive ? styles : {}}
+            >
               My Profile
             </NavLink>
           </li>
