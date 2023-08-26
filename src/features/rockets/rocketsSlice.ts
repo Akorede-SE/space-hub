702351cede442fe4bc2e0a3eb8cf87c81be3a6c3
akorede <emiolasaheed1@gmail.com>
2023-08-26 11:08:32 +0100

    Fecth and consume missions data

--- src/features/rockets/rocketsSlice.ts
+++ src/features/rockets/rocketsSlice.ts
@@ -1,11 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 export type initialStateProp = {
-  rockets: [],
+  rockets: RocketsProp[],
   loading: boolean,
-  error: string,
+  error: string | undefined,
 }
-//description id name flickr_images[]
 const initialState: initialStateProp = {
   rockets: [],
   loading: false,
@@ -15,16 +14,41 @@ const initialState: initialStateProp = {
 export const getRockets = createAsyncThunk('rockets/getRockets', async () => {
   try {
     const response = await axios.get('https://api.spacexdata.com/v4/rockets');
-    const { data } = response;
+    const { data } = response;    
     return data;
   } catch (error) {
     return error;
   }
 })
+export type RocketsProp = {
+  name: string,
+  description: string,
+  id: string,
+  flickr_images: string[],
+}
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(getRockets.pending, (state) => {
+      state.loading = true;
+    })
+    .addCase(getRockets.fulfilled, (state, action) => {
+      state.loading = false;
+      const data = action.payload.map((item: RocketsProp) => {        
+        const {name, description, id,  flickr_images} = item;
+        return {name, description, id, flickr_images}
+      })
+      state.rockets = data;
+      state.error = '';
+    })
+    .addCase(getRockets.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message
+    })
+  }
 });
 
 export default rocketsSlice.reducer;
